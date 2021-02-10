1ad686e027848287adbe47f0b998cb5ae7c098ad
Simon Koelewijn <simon@seems.nl>
2021-02-10 15:18:14 +0100

    Change sites

--- _data/sites/eftelingcom.js
+++ _data/sites/eftelingcom.js
@@ -3,10 +3,9 @@ module.exports = {
 	description: "Efteling.com",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
+		freshChrome: "site"
 	},
 	urls: [
 		"https://www.efteling.com",
-		"https://www.efteling.com/nl/park/reserveer-bezoek",
-		"https://www.efteling.com/nl/overnachten"
 	]
 };
