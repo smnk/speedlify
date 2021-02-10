ff362073f6eaa6e6536520a22e5e8e201abb8255
Simon Koelewijn <simon@seems.nl>
2021-02-10 15:01:24 +0100

    Add efteling.com pages

---
+++ _data/sites/eftelingcom.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Efteling.com", // optional, falls back to object key
+	description: "Efteling.com",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.efteling.com",
+		"https://www.efteling.com/nl/park/reserveer-bezoek",
+		"https://www.efteling.com/nl/overnachten"
+	]
+};
